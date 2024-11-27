7ec69853dde4722a8ea1385cd4089c10451dea4b
Dylan Schumacker <Dylan.schumacker@outlook.fr>
2024-11-27 12:39:27 +0100

    Updated the util route-extension

--- front-end/src/app/shared/utils/router-extension.ts
+++ front-end/src/app/shared/utils/router-extension.ts
@@ -1,6 +1,3 @@
-import { inject } from '@angular/core';
-import { Router } from '@angular/router';
-
 export interface RouteData {
   path: string;
   title: string;
@@ -14,6 +11,14 @@ export class RouterExtension {
   public static readonly EXERCISES = 'exercises';
   public static readonly SESSION = 'session';
 
+  /**
+   * Route data | Defines all the app's routes, it's used to reference them in different parts of the application.
+   * @param path The path of the route.
+   * @param title The title of the route (the page's name).
+   * @param icon The icon is used in the sidebar or relative components that need an icon to be displayed.
+   * @param order The order is used to sort the routes.
+   * @example const homeRoute: RouteData = { path: 'home', title: 'Home', icon: 'assets/icons/home.svg', order: 1 };
+   */
   private static readonly _routesData: Array<RouteData> = [
     {
       path: RouterExtension.NOT_FOUND,
@@ -39,7 +44,15 @@ export class RouterExtension {
     },
   ];
 
-  public static getRoutesData() {
+  /**
+   * Gets the app's routes data in the correct order.
+   *
+   * The first element of the `_routesData` array is the WILDCARD route, so it's removed.
+   * The routes are then sorted by their order property.
+   *
+   * @returns {Array<RouteData>} The sorted routes data.
+   */
+  public static getRoutesData(): Array<RouteData> {
     if (RouterExtension._routesData.length === 0) {
       console.warn('Routes._routesData is empty');
       return [];
@@ -57,20 +70,23 @@ export class RouterExtension {
     });
   }
 
+  /**
+   * Gets a route by path.
+   *
+   * @param {string} path The route's path.
+   * @returns {RouteData | undefined} The route if found, undefined otherwise.
+   */
   public static getRouteByPath(path: string): RouteData | undefined {
     return RouterExtension._routesData.find((route) => route.path === path);
   }
 
+  /**
+   * Gets a route by title.
+   *
+   * @param {string} title The route's title.
+   * @returns {RouteData | undefined} The route if found, undefined otherwise.
+   */
   public static getRouteByTitle(title: string): RouteData | undefined {
     return RouterExtension._routesData.find((route) => route.title === title);
-  }
-
-  public static defineRouteOrder(path: string, order: number) {
-    const route = RouterExtension.getRouteByPath(path);
-    if (!route || route.order === order) {
-      return;
-    }
-
-    route.order = order;
   }
 }
