c1691ca849056e8d9654c79176dda4f8283a9da8
Dylan Schumacker <Dylan.schumacker@outlook.fr>
2024-12-09 14:48:24 +0100

    fix: removed unused interface

--- front-end/src/app/shared/interfaces/filterable.ts
+++
@@ -1,6 +0,0 @@
-import { IFilter } from '../components/filter/filter-item/filter-item.component';
-
-export interface IFilterable<T extends string | number | Date> {
-  appliedFilters: Array<IFilter<T>>;
-  handleFilterSelection(eventData: Array<IFilter<T>>): void;
-}
