b16584096bde98cd25865a4b4a887aea943331ad
Dylan Schumacker <Dylan.schumacker@outlook.fr>
2024-12-09 14:52:16 +0100

    feat: work on series validator

--- front-end/src/app/features/exercise/series-validator/series-validator.component.ts
+++ front-end/src/app/features/exercise/series-validator/series-validator.component.ts
@@ -3,6 +3,8 @@ import { NumberToArrayPipe } from '../../../shared/pipes/number-to-array/number-
 import { IconComponent } from '../../../shared/components/icon/icon.component';
 import { Icon } from '../../../shared/components/icon/icon.interface';
 import { IInitializable } from '../../../shared/interfaces/initializable';
+import { Subscription } from 'rxjs';
+import { ExtendedArray } from '../../../shared/extensions/extended-array';
 
 @Component({
   selector: 'app-series-validator',
@@ -12,12 +14,15 @@ import { IInitializable } from '../../../shared/interfaces/initializable';
   styleUrl: './series-validator.component.css',
 })
 export class SeriesValidatorComponent implements IInitializable {
-  @Input({ required: true })
-  amount: number = 0;
+  @Input({ required: false }) amount: number = 0;
   @ViewChildren(IconComponent)
   icons: QueryList<IconComponent> = new QueryList();
 
-  private readonly _series: { icon: Icon; hasBeenValidated: boolean }[] = [];
+  private _iconsChangesSubscription = new Subscription();
+  private readonly _series = new ExtendedArray<{
+    icon: Icon;
+    hasBeenValidated: boolean;
+  }>();
 
   idleSeriesIcon: string = 'assets/icons/circle.png';
   validSeriesIcon: string = 'assets/icons/circle-full.png';
@@ -28,23 +33,52 @@ export class SeriesValidatorComponent implements IInitializable {
     this.init();
   }
 
-  init(): void {
-    this.icons.forEach((i) => {
-      this._series.push({
-        icon: i,
-        hasBeenValidated: false,
-      });
-    });
+  ngOnDestroy(): void {
+    this._iconsChangesSubscription.unsubscribe();
+  }
 
-    console.log(this._series);
+  init(): void {
+    // console.log('[init] - Amount : ', amount);
+    if (this.amount > 0) return;
+
+    this._iconsChangesSubscription = this.icons.changes.subscribe(
+      (queryList: QueryList<IconComponent>) => {
+        queryList.forEach((icon) => {
+          this._series.push({
+            icon: icon,
+            hasBeenValidated: false,
+          });
+        });
+
+        // console.log('[init] - Series : ', this._series);
+      }
+    );
   }
 
   validateOneSeries() {
+    if (this.areAllSeriesValidated()) return; // No need to validate if all series are valid
+
+    console.log('[validateOneSeries] - Validating one series');
+
+    const firstUnvalidatedSeries = this._series.find(
+      (s) => !s.hasBeenValidated
+    );
+    if (firstUnvalidatedSeries) {
+      firstUnvalidatedSeries.hasBeenValidated = true;
+      firstUnvalidatedSeries.icon.setIcon(this.validSeriesIcon);
+    }
+  }
+
+  areAllSeriesValidated(): boolean {
+    return this._series.every((s) => s.hasBeenValidated);
+  }
+
+  clear() {
     this._series.forEach((s) => {
-      if (!s.hasBeenValidated) {
-        s.hasBeenValidated = true;
-        s.icon.setIcon(this.validSeriesIcon);
-      }
+      s.hasBeenValidated = false;
+      s.icon.setIcon(this.idleSeriesIcon);
     });
+
+    console.log('[clear] - Series cleared', this._series);
   }
 }
